0dfb598c903b1c00c58bf57d01809e63759f06d4
pSbral <rm87990@fiap.com.br>
2025-11-15 15:00:03 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/SetisAvaliacaoTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetisAvaliacaoTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetisAvaliacaoTecnicaApplication.class, args);
+	}
+
+}
