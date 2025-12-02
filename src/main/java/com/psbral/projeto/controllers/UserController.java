959a84da0e5c0671dc27e68f28ac876e09ebe121
psbral <s0bralcontact@gmail.com>
2025-12-02 14:54:59 -0300

    Refatoração de Código - Remoção de "@Transactional" - Inclusão de "final"

--- src/main/java/com/psbral/projeto/controllers/UserController.java
+++ src/main/java/com/psbral/projeto/controllers/UserController.java
@@ -4,23 +4,23 @@ import com.psbral.projeto.models.User;
 import com.psbral.projeto.services.UserService;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
     @Autowired
-    private UserService service;
+    private final UserService service;
 
     // CREATE
     // POST /users
     @PostMapping
-    @Transactional
     public User insert(@Valid @RequestBody User user) {
         return service.insert(user);
     }
@@ -28,7 +28,6 @@ public class UserController {
     // READ – FIND ALL
     // GET /users
     @GetMapping
-    @Transactional(readOnly = true)
     public List<User> findAll() {
         return service.findAll();
     }
@@ -36,7 +35,6 @@ public class UserController {
     // READ – FIND BY ID
     // GET /users/{id}
     @GetMapping("/{id}")
-    @Transactional(readOnly = true)
     public User findById(@PathVariable Long id) {
         return service.findById(id);
     }
@@ -44,7 +42,6 @@ public class UserController {
     // UPDATE
     // PUT /users/{id}
     @PutMapping("/{id}")
-    @Transactional
     public User update(@PathVariable Long id,
                        @Valid @RequestBody User user) {
         return service.update(id, user);
@@ -53,7 +50,6 @@ public class UserController {
     // DELETE
     // DELETE /users/{id}
     @DeleteMapping("/{id}")
-    @Transactional
     public void delete(@PathVariable Long id) {
         service.delete(id);
     }
